1296f3cdbef4a2c895c0dd03e94bd7b2a5848c72
siopipin <siojurnalispipin@hotmail.com>
2023-10-16 11:43:32 +0700

    learn js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 function App() {
+  const myMap = new Map()
+  myMap.set('1', 'value1')
+  myMap.set('2', 'value2')
+
+  const arr = [1, 2, 3, 4, 5]
+  arr.map((item,i) => {console.log(`index ke-${i}: ${item}`);})
+
+  console.log(myMap);
+  console.log(myMap.get('1'));
+
   return (
     <div>
       <h2>Let's get started!</h2>
