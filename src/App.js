4b237d8448b2de43475865d9ab36612125f23869
siopipin <siojurnalispipin@hotmail.com>
2024-12-20 17:13:11 +0700

    versi sederhana axios dengan data localhost

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import M07App from "./M07/M07App";
+// import M07App from "./M07/M07App";
+import M13Page from "./M13/M13Page";
 
 function App() {
   return (
-    <M07App />
+    <M13Page />
   );
 }
 
---
+++ src/M13/M13Page.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function M13Page() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/web/users");
+      setUsers(response.data.results);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading.....</div>;
+  }
+
+  return (
+    <div>
+      <h1>Data User</h1>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <b>Name:</b> {user.name}
+              <br />
+              <b>Email:</b> {user.email}
+              <br />
+              <b>Address:</b> {user.address}
+              <br />
+              <b>Created:</b> {user.createdAt}
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default M13Page;
