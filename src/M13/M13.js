153e3817e626f8bd99d06d138a9a1513984638e2
siopipin <siojurnalispipin@hotmail.com>
2024-12-20 18:15:37 +0700

    url

--- src/M13/M13.js
+++ src/M13/M13.js
@@ -1,8 +1,28 @@
-export const M13 = () => {
-    // axios get data dari local
-    // url= https://8c04-103-165-239-161.ngrok-free.app
+import axios from "axios";
+import { useEffect } from "react";
 
-    return <div>
-        <h1>Hallo</h1>
+export const M13 = () => {
+  // axios get data dari local
+  // url= https://8c04-103-165-239-161.ngrok-free.app
+
+  const ambilDataServer = async () => {
+    try {
+      const response = await axios.get(
+        "https://8c04-103-165-239-161.ngrok-free.app/web/users"
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    ambilDataServer();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hallo</h1>
     </div>
-}
+  );
+};
